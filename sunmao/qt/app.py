8afa99bd5ed0d14ea8aab1d874c33dff67fc3f24
nanguage <nanguage@yahoo.com>
2023-07-24 02:47:29 +0800

    update

--- sunmao/qt/app.py
+++ sunmao/qt/app.py
@@ -4,6 +4,7 @@ import typing as T
 from qtpy import QtWidgets
 from easynode.node_editor import NodeEditor
 from easynode.setting import EditorSetting
+from sunmao.api import Session, Flow, compute
 
 from .ui.menubar import Menubar
 
@@ -11,11 +12,15 @@ from .ui.menubar import Menubar
 class SunmaoQt(QtWidgets.QWidget):
     current_instance = None
 
-    def __init__(self, editor_setting: T.Optional[EditorSetting] = None):
+    def __init__(
+            self,
+            session: Session,
+            editor_setting: T.Optional[EditorSetting] = None):
         super().__init__()
         self._editor_setting = editor_setting
         self.__class__.current_instance = self
         self._init_ui()
+        self.session = session
 
     def _init_ui(self):
         self.layout = QtWidgets.QVBoxLayout(self)
@@ -26,9 +31,25 @@ class SunmaoQt(QtWidgets.QWidget):
         self.layout.setMenuBar(self.menubar)
         self.layout.addWidget(self.node_editor)
 
+    def _init_node_editor_from_session(self):
+        # TODO: init node editor from session
+        pass
+
 
 if __name__ == "__main__":
+    @compute
+    def Add(a: int, b: int) -> int:
+        return a + b
+
+    with Session() as session:
+        with Flow() as flow:
+            add1 = Add(name="add1")
+            add2 = Add(name="add2")
+            add3 = Add(name="add3")
+            add1.O[0] >> add3.I[0]
+            add2.O[0] >> add3.I[1]
+
     app = QtWidgets.QApplication(sys.argv)
-    instance = SunmaoQt()
+    instance = SunmaoQt(session)
     instance.show()
     app.exec_()
